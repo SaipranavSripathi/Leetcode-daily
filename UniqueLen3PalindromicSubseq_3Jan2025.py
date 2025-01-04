3f6cd08c0c476d0ccac5f235f025706ea2e49fb2
Sripathi Sai Pranav <pranavsripathi2106@gmail.com>
2025-01-03 19:11:43 -0800

    Unique num of palindromic sun sequences of length 3

---
+++ UniqueLen3PalindromicSubseq_3Jan2025.py
@@ -0,0 +1,24 @@
+class Solution:
+    def countPalindromicSubsequence(self, s: str) -> int:
+        first = [-1] * 26
+        last = [-1] * 26
+        
+        for i in range(len(s)):
+            curr = ord(s[i]) - ord("a")
+            if first[curr] == -1:
+                first[curr] = i
+            
+            last[curr] = i
+        
+        ans = 0
+        for i in range(26):
+            if first[i] == -1:
+                continue
+                
+            between = set()
+            for j in range(first[i] + 1, last[i]):
+                between.add(s[j])
+            
+            ans += len(between)
+
+        return ans
